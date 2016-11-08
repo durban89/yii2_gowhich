b379fc8c427dce1d45d09cfc6a7265e51a3ef2a0
dapeng126 <zhangdapeng89@126.com>
2016-11-08 18:14:07 +0800

    配置参数

--- config/params.php
+++ config/params.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
-    'name'       => 'Gowhich',
+    'adminEmail'  => 'admin@example.com',
+    'name'        => 'Gowhich',
+    'title'       => '技术人生',
+    'description' => '互联网开发、知识学习、实例展示、技能成长',
 ];
