ab86e4b9f8a800aa6ceb3c51f65abb8b8d84d7d6
dapeng126 <zhangdapeng89@126.com>
2016-11-10 21:38:24 +0800

    添加 路由

--- config/router.php
+++ config/router.php
@@ -1,5 +1,21 @@
 <?php
 
 return [
+    ''                        => 'site/index',
+    'admin'                   => 'admin/site/login',
+    'admin/logout'            => 'admin/site/logout',
 
+    'blog'                    => 'site/index',
+    'blog/<id:\d+>'           => 'site/view',
+    'blog/view/id/<id:\d+>'   => 'site/view',
+    'blog/view'               => 'site/view',
+    'blog/<id:.*?>'           => 'site/view',
+    'category'                => 'category/index',
+    'category/<category:.*?>' => 'category/view',
+    'tag'                     => 'tag/view',
+    'tag/<tag:.*?>'           => 'tag/index',
+    'search/'                 => 'search/index',
+    'type/search/<id:\d+>'    => 'search/index',
+    'author/<author:.*?>'     => 'author/index',
+    'author'                  => 'author/view',
 ];
