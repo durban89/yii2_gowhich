e2e28fb95ae83badce847a48cc2e851b1c4b4302
dapeng126 <zhangdapeng89@126.com>
2016-11-12 22:56:15 +0800

    修改文章的作者链接

--- components/views/article.php
+++ components/views/article.php
@@ -19,7 +19,7 @@ $this->registerJsFile('//gowhich.disqus.com/count.js', ['async' => true, 'id' =>
             <ul class="list-inline entry-meta">
                 <li class="meta-date"> Posted on <a href="https://wp-themes.com/?p=8" rel="date">
                 <time datetime="<?=date('c', strtotime($v->create_date));?>" class="timeago"><?=date('m-d,Y', strtotime($v->create_date));?></time></a></li>
-                <li class="meta-author"> by <a href="<?=Url::to(['author/index', 'author' => $v->user->username]);?>" rel="author"><?=$v->user->username;?></a></li>
+                <li class="meta-author"> by <a href="<?=Url::to(['author/view', 'author' => $v->user->username]);?>" rel="author"><?=$v->user->username;?></a></li>
             </ul>
        </p>
     </header>
