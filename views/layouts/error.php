39b98fa8d73f884961a3b1e981f4889176d86d00
dapeng126 <zhangdapeng89@126.com>
2016-11-18 16:25:51 +0800

    添加 error layout

---
+++ views/layouts/error.php
@@ -0,0 +1,46 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use app\assets\AppAsset;
+use app\components\FooterWidget;
+use app\components\HeaderWidget;
+use yii\helpers\Html;
+use yii\widgets\Breadcrumbs;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage();?>
+<!DOCTYPE html>
+<html lang="<?=Yii::$app->language;?>">
+<head>
+    <meta charset="<?=Yii::$app->charset;?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?=Html::csrfMetaTags();?>
+    <title><?=Html::encode($this->title);?></title>
+    <?=$this->head();?>
+</head>
+<body>
+<?php $this->beginBody();?>
+
+<?=HeaderWidget::widget();?>
+
+<div class="wrap">
+    <div class="container">
+        <?=Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : []]);?>
+        <div class="row">
+            <div class="main-content col-md-12" role="main">
+            <?=$content;?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?=FooterWidget::widget();?>
+
+
+<?php $this->endBody();?>
+</body>
+</html>
+<?php $this->endPage();?>
