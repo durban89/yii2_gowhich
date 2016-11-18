e4106ba5d7b69391d42408be4a93f1b9ac7fe8dd
dapeng126 <zhangdapeng89@126.com>
2016-11-19 07:10:17 +0800

    添加 archives

--- controllers/ArchivesController.php
+++ controllers/ArchivesController.php
@@ -2,11 +2,20 @@
 
 namespace app\controllers;
 
+use app\models\Blog;
+
 class ArchivesController extends \yii\web\Controller
 {
     public function actionIndex()
     {
+        $model = Blog::find()->where()->all();
         return $this->render('index');
+    }
+
+    public function actionView($year, $date)
+    {
+        $model = Blog::find()->where()->all();
+        return $this->render('view');
     }
 
 }
