32cc338f82dd596a2e1f662912728ca980a47500
dapeng126 <zhangdapeng89@126.com>
2016-11-20 22:37:51 +0800

    添加 百度 统计

---
+++ components/StatBaiduWidget.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\components;
+
+use yii\base\Widget;
+
+class StatBaiduWidget extends Widget
+{
+
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function run()
+    {
+        return $this->render('stat_baidu');
+    }
+}
