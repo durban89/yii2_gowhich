f3964d3d203c37663ef7d98afb318b9229f7b57d
dapeng126 <zhangdapeng89@126.com>
2016-11-21 22:52:19 +0800

    修改 关于我 页面

--- views/site/about.php
+++ views/site/about.php
@@ -2,17 +2,15 @@
 
 /* @var $this yii\web\View */
 
-use yii\helpers\Html;
-
-$this->title = 'About';
-$this->params['breadcrumbs'][] = $this->title;
+$this->title                   = '关于我' . ' - ' . Yii::$app->params['name'];
+$this->params['breadcrumbs'][] = 'About';
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1>关于我</h1>
 
     <p>
         This is the About page. You may modify the following file to customize its content:
     </p>
 
-    <code><?= __FILE__ ?></code>
+
 </div>
