022bb458af595e60e10ab199ce711c8cf0b38635
dapeng126 <zhangdapeng89@126.com>
2016-11-21 22:52:05 +0800

    添加 社交圈 页面

---
+++ views/site/social.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title                   = '社交圈' . ' - ' . Yii::$app->params['name'];
+$this->params['breadcrumbs'][] = 'Social Hub';
+?>
+<div class="site-about">
+    <h1>社交圈</h1>
+
+    <p>
+        This is the About page. You may modify the following file to customize its content:
+    </p>
+
+
+</div>
